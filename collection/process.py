b2067133af52190954c7b6a1a3578d35c5c1e344
Kevin Quinn <quinnk@scc1.bu.edu>
2025-06-18 17:05:04 -0400

    experiment results

--- collection/process.py
+++ collection/process.py
@@ -1,9 +1,12 @@
 from grobid_client.grobid_client import GrobidClient
+import os 
+from dotenv import load_dotenv
+load_dotenv()
 
 client = GrobidClient(config_path="config.json")
 client.process(
     service="processFulltextDocument",
     input_path="collection/pdfs",
-    output="collection/processed2",
-    n=1
+    output="collection/processed",
+    n=3
 )
--- collection/search.py
+++ collection/search.py
@@ -6,7 +6,7 @@ import requests
 import time
 load_dotenv()
 
-n_papers = 200
+n_papers = 1200
 search_parameters = {
     "query" : "",
     "bulk" : True,
@@ -18,7 +18,7 @@ sch = SemanticScholar()
 # Search and get list of DOIs
 response = sch.search_paper(**search_parameters)
 dois = []
-for paper in response[120:180]:
+for paper in response:
     doi = paper['externalIds'].get('DOI')
     if doi is not None:
         dois.append(doi)
@@ -31,6 +31,7 @@ headers = {
     "Wiley-TDM-Client-Token": api_key
 }
 
+save_directory = os.getenv("PDF_PATH")
 for doi in dois:
     # Construct the URL for the specific DOI and make the request
     url = f"{base_url}{doi}"
@@ -38,7 +39,7 @@ for doi in dois:
 
     # Save PDF
     if response.status_code == 200:
-        fname = 'collection/pdfs/' + doi.replace('/', '_') + '.pdf'
+        fname = save_directory + doi.replace('/', '_') + '.pdf'
         with open(fname, "wb") as pdf_file:
             pdf_file.write(response.content)
         print(f"Downloaded PDF for DOI: {doi} to {fname}")
@@ -46,3 +47,5 @@ for doi in dois:
         print(f"Failed to download PDF. Status code: {response.status_code}")
         print("Faied DOI:", doi)
         print(response.text)
+
+    time.sleep(10)
