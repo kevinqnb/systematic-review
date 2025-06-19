c868ffcce6a1a6eb0c17380c2d0442f37e78a1a3
Kevin Quinn <quinnk@scc1.bu.edu>
2025-06-19 08:51:29 -0400

    experiment results

--- collection/process.py
+++ collection/process.py
@@ -3,10 +3,12 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+inpath = os.getenv("PDF_PATH")
+outpath = os.getenv("PROCESSED_PATH")
 client = GrobidClient(config_path="config.json")
 client.process(
     service="processFulltextDocument",
-    input_path="collection/pdfs",
-    output="collection/processed",
+    input_path=inpath,
+    output=outpath,
     n=3
 )
